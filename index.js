984b3323f44603c484958f9b82ce92959dbf94b9
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-02-26 20:35:10 -0700

    Added content

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const container = document.getElementById("grid");
 
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+  const gridItem = document.getElementsByClassName("grid-item");
+  gridItem.style.width = "100px" ;
+}; 
+
+makeRows(16, 16);
