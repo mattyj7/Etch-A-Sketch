68c2b9457e3614372a47e7f13d4b29c174e2c3d9
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-03-01 20:25:26 -0700

    finished main code for etach-a-sketch. fully functional

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const output = document.getElementById("output");
 const range = document.getElementById("myRange").step = "2";
 const value = document.getElementById("myRange").value
 const slider = document.getElementById("myRange");
+const resetBtn = document.getElementById("reset");
+const rainbow = document.getElementById("rainbow");
+const black = document.getElementById("black");
 
 
+let mode = true;
 
 function reset () {
   let grid = document.querySelectorAll(".grid");
@@ -24,22 +28,41 @@ function makeRows(rows, cols) {
     let cell = document.createElement("div");
     cell.classList.add("grid");
     cell.addEventListener('mouseover', changeColor);
-    cell.addEventListener('mousedown', changeColor)
+    // cell.addEventListener('mousedown', changeColor)
     container.appendChild(cell);
   };
   reset();
 }; 
 
 function changeColor (e) {
+  if(mode === true) {
   e.target.style.backgroundColor = 'black';
+  }
+  else if (mode === false) {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+  }
 }
 
-function changeColor (e) {
-  e.target.style.backgroundColor = 'black';
+function defaultReset () {
+  makeRows(slider.value, slider.value)
 }
 
-output.innerHTML = slider.value + " x " + slider.value;
+function changeMode () {
+  mode = false;
+}
+
+function modeDefault () {
+  mode = true;
+}
+
+output.innerHTML = slider.value + " x " + slider.value + " pixels";
 makeRows(4, 4)
-slider.oninput =function(){output.innerHTML = slider.value + " x " + slider.value;}
+slider.oninput =function(){output.innerHTML = slider.value + " x " + slider.value + " pixels";}
 
 slider.addEventListener('mouseup', function(){ makeRows(slider.value, slider.value)});
+resetBtn.addEventListener('mousedown', function(){ makeRows(slider.value, slider.value)});
+rainbow.addEventListener('click', changeMode, true);
+black.addEventListener('click', modeDefault, true);
